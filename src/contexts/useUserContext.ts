881e36a2f7dd7809327324284737c2edc1c9846a
Chukwuemeka <mekastans@gmail.com>
2025-04-28 14:35:08 +0100

    Modify code for deployment

--- src/contexts/useUserContext.ts
+++ src/contexts/useUserContext.ts
@@ -1,13 +1,12 @@
 import React, { createContext, useContext } from 'react';
 import { useUserSession } from './useUserSession';
 
-const UserSessionContext = createContext<(ReturnType<typeof useUserSession> | null)>(null);
+const UserSessionContext = createContext<ReturnType<typeof useUserSession> | null>(null);
 
 export const UserSessionProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
   const userSession = useUserSession();
-  
+
   return (
-    //@ts-ignore
     <UserSessionContext.Provider value={userSession}>
       {children}
     </UserSessionContext.Provider>
@@ -21,27 +20,3 @@ export const useUserSessionContext = () => {
   }
   return context;
 };
-
-// import React, { createContext, useContext } from 'react';
-// import { useUserSession } from './useUserSession';
-
-// const UserSessionContext = createContext<ReturnType<typeof useUserSession> | null>(null);
-
-// export const UserSessionProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
-//   const userSession = useUserSession();
-  
-//   return (
-//     //@ts-ignore
-//     <UserSessionContext.Provider value={userSession}>
-//       {children}
-//     </UserSessionContext.Provider>
-//   );
-// };
-
-// export const useUserSessionContext = () => {
-//   const context = useContext(UserSessionContext);
-//   if (!context) {
-//     throw new Error('useUserSessionContext must be used within a UserSessionProvider');
-//   }
-//   return context;
-// };
